ec9494c6c9fe6a9b4d5aae27757ef3fca4ac353a
benjibowe20 <94123014+benjibowe20@users.noreply.github.com>
2022-03-08 23:42:52 +0000

    Add files via upload
    Made minor changes to code, added some comments and played around with GUI to try and work out a layout I'm happy with. I also created a class for file I/O but have nothing in the class yet, was only to build more of a structure and lay things out

--- assignment/GUI.java
+++ assignment/GUI.java
@@ -1,48 +1,94 @@
+/*
+ * Assignment Package
+ */
 package assignment;
 
+/*
+ * All used imports listed here for use in class
+ */
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
 import javax.swing.*;
 
+/*
+ * Class used for the generation and interaction of GUI
+ * Contains: 
+ * 		-
+ * 		-
+ */
 public class GUI extends JFrame implements ActionListener{
 	
+	/*
+	 * All javax.swing variables declared here
+	 */
 	JLabel label1, label2;
 	JButton button1, button2;
 	JPanel panel1, panel2;
 	JTextField textfield;
-
+	JFrame frame;
 	
+	/*
+	 * Regular data type variables are declared here
+	 */
+	//String[] gender = {"<html><font size = '5'>Male", "<html><font size = '5'>Female"};
+	
+	/*
+	 * Generation of GUI, Action Listeners...
+	 */
 	public GUI(String title) {
+		
 		super (title);
+		
+		JFrame frame = new JFrame();
+		setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+		frame.setAlwaysOnTop(true);
 		setVisible(true);
+	//	setSize(500,500);
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setLayout(new FlowLayout());
+		
 		JPanel panel1 = new JPanel();
 		JPanel panel2 = new JPanel();
 		add(panel1);
-		panel1.setLayout(new FlowLayout());
 		add(panel2);
-		panel2.setLayout(new FlowLayout());
+		
+		JLabel label1 = new JLabel("<html><font size = '4'>What is the gender of the student?</html>");
+		panel1.add(label1);
 		
 		
 		
-		JLabel label1 = new JLabel("<html><font size = '5'>What is the gender of the student?");
-		add(label1);
+		
+//		JComboBox<String> genderBox = new JComboBox<>(gender);
+//      panel2.add(genderBox);
+//      genderBox.addActionListener(this);
+		
+		
 		
 		button1 = new JButton();
 		panel2.add(button1);
-		button1.setText("<html><font size = '5'>  Male");
+		button1.setText("<html><font size = '4'>  Male</html>");
 		button1.setToolTipText("This button generates a random number");
 		button1.addActionListener(this);
+		button1.setPreferredSize(new Dimension(100,35));
 		
 		button2 = new JButton();
 		panel2.add(button2);
-		button2.setText("<html><font size = '5'>Female");
+		button2.setText("<html><font size = '4'>  Female</html>");
 		button2.setToolTipText("This button generates a random number");
 		button2.addActionListener(this);
+		button2.setPreferredSize(new Dimension(100,35));
+		
+//		JLabel label2 = new JLabel("<html><font size = '4'>Gender: </html>");
+//		panel1.add(label2);
 		
 
 		}
@@ -52,9 +98,8 @@ public class GUI extends JFrame implements ActionListener{
 		{
 			if(e.getSource() == button1)
 			{
-				JOptionPane.showMessageDialog(this, "Male");
+				//label2.setText("<html><font size = '4'>Gender: Male</html>");
 			}
 			
 		}
-
 }
--- assignment/main.java
+++ assignment/main.java
@@ -1,11 +1,28 @@
-package assignment;
-import javax.swing.*;
+/*Object Oriented Programming Assignment 2022.
+ * Machine Learning Model, using Naive Bayes.
+ * Author: Benji Bowe
+ * Student Number: C20416006
+ * Last edited: 08/03/2022
+ */
 
+/*
+ * Assignment Package
+ */
+package assignment;
+
+/*
+ * Main Class for control of all other classes.
+ */
 public class main {
 	
 	public static void main(String[] args) {
 		
+		/*
+		 * Calls GUI from GUI.java for user to interact with application
+		 */
 		GUI mine = new GUI("Machine Learning Model");
+		
+		
 	}
-
 }
+//END
